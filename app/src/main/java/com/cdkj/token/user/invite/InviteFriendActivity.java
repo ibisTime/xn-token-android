976e7bd0c0476815559a299467c5da051bf564ae
theredheart <1559989095@qq.com>
2018-08-08 22:07:32 +0800

    邀请好友界面、海报界面

---
+++ app/src/main/java/com/cdkj/token/user/invite/InviteFriendActivity.java
@@ -0,0 +1,28 @@
+package com.cdkj.token.user.invite;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.cdkj.baselibrary.base.AbsLoadActivity;
+
+/**
+ * 邀请有礼
+ * Created by cdkj on 2018/8/8.
+ */
+
+public class InviteFriendActivity extends AbsLoadActivity {
+
+
+
+    @Override
+    public View addMainView() {
+        return null;
+    }
+
+    @Override
+    public void afterCreate(Bundle savedInstanceState) {
+
+    }
+
+
+}
